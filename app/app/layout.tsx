4d12ee3c1ec7b8bfaebec2dc0cedb5cfc0e7b6a3
thwalasiyabonga17 <thwalasiyabonga757@gmail.com>
2025-12-03 09:44:50 +0200

    Create layout.tsx

---
+++ app/app/layout.tsx
@@ -0,0 +1,28 @@
+import './globals.css';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { clusterApiUrl } from '@solana/web3.js';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = clusterApiUrl(network);
+  const wallets = [new PhantomWalletAdapter()];
+
+  return (
+    <html lang="en">
+      <body className="bg-black text-white">
+        <ConnectionProvider endpoint={endpoint}>
+          <WalletProvider wallets={wallets} autoConnect>
+            <WalletModalProvider>{children}</WalletModalProvider>
+          </WalletProvider>
+        </ConnectionProvider>
+      </body>
+    </html>
+  );
+}
